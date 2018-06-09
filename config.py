e377959bb4dccf6483ba990a1a05c4499d6f63e4
Andrew Parrott <aparrott@blizzard.com>
2018-06-09 09:07:19 -0500

    first commit

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = "G6ZqRrAitjcqmvWukiLGfv8Cg"
+consumer_secret = "yxKoLHuDJ8oRnnTucqHANGgIYgHEelNScOakHqccv3PSK8esr3"
+access_token = "30689561-8829As2IlKcfsKVqRav8jiNkMVe8q6JckcAAav4jk"
+access_token_secret = "LgC5ETSTf5Z2SmaXjS0JyVKQE0YY4FAe56GDPUfeGFLO0"
